bf1072ac5dd39cf58f64be2224d64e83c30bd242
Korexio <korexio@gmail.com>
2023-11-11 07:20:59 +0100

    Rename logger extension methods

--- Source/Korexio.Prototype/Source/Korexio.Prototype/LoggerExtensionMethods.cs
+++ Source/Korexio.Prototype/Source/Korexio.Prototype/LoggerExtensionMethods.cs
@@ -5,11 +5,11 @@ namespace Korexio.Prototype;
 public static partial class LoggerExtensionMethods
 {
   [LoggerMessage(EventId = 100, Message = "'{Name}' started.")]
-  public static partial void Started(this ILogger logger, string name, LogLevel level = LogLevel.Information);
+  public static partial void LogStarted(this ILogger logger, string name, LogLevel level = LogLevel.Information);
 
   [LoggerMessage(EventId = 200, Message = "'{Name}' stopped.")]
-  public static partial void Stopped(this ILogger logger, string name, LogLevel level = LogLevel.Warning);
+  public static partial void LogStopped(this ILogger logger, string name, LogLevel level = LogLevel.Warning);
 
   [LoggerMessage(EventId = 300, Message = "Using configuration value '{Name}' = '{Value}'.")]
-  public static partial void UsingConfigurationValue(this ILogger logger, string name, object value, LogLevel level = LogLevel.Debug);
+  public static partial void LogUsingConfigurationValue(this ILogger logger, string name, object value, LogLevel level = LogLevel.Debug);
 }
